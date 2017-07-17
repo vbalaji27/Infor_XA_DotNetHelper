6583ff51fee7651d2f871e8fee01eea98a7afd05
Balaji` <vvbalaji@users.noreply.github.com>
2017-07-17 13:19:22 +0530

    First commit - adding to git

---
+++ Infor_XA_DotNetHelper/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infor_XA_DotNetHelper
+{
+    public class Class1
+    {
+    }
+}
